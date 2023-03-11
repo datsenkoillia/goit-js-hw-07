4ca503f62b91b83124bb5ac092e2790a3104cc0f
Illia Datsenko <datsenko.i@gmail.com>
2023-03-11 19:30:18 +0200

    test

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -65,3 +65,53 @@ function showBigPictureCreate(event) {
   );
   return instance;
 }
+
+// const galleryContainer = document.querySelector('.gallery');
+
+// addGalleryToHtml(galleryContainer);
+
+// function createGallery(arr) {
+//   return arr
+//     .map(({ preview, original, description }) => {
+//       return `<div class="gallery__item">
+//                 <a class="gallery__link" href="${original}">
+//                     <img
+//                     class="gallery__image"
+//                     src="${preview}"
+//                     data-source="${original}"
+//                     alt="${description}"
+//                     />
+//                 </a>
+//                 </div>`;
+//     })
+//     .join('');
+// }
+
+// function addGalleryToHtml(container) {
+//   container.innerHTML = createGallery(galleryItems);
+// }
+
+// galleryContainer.addEventListener('click', handleLargeImage);
+
+// function handleLargeImage(evt) {
+//   evt.preventDefault();
+//   if (evt.target.nodeName !== 'IMG') {
+//     return;
+//   }
+
+//   const instance = basicLightbox.create(`
+// 	<img src="${evt.target.dataset.source}" width="800" height="600">
+// `);
+
+//   instance.show();
+
+//   document.addEventListener(
+//     'keydown',
+//     evt => {
+//       if (evt.code === 'Escape') {
+//         instance.close();
+//       }
+//     },
+//     { once: true }
+//   );
+// }
